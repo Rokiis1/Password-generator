0cd0ba5d23240351351709faa5a381fb1d47888c
Rokiis1 <rokasnorviliss@gmail.com>
2022-12-16 10:22:23 +0200

    you can choose what you wanna in your password

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 import secrets
+import random
 import string
 
-def generate_password(length):
+def generate_password(length, letters=True, digits=True, punctuation=True):
     """Generate a secure random password with the given length"""
-    # Get a list of all the ASCII characters
-    characters = string.ascii_letters + string.digits + string.punctuation
-    # Generate a secure random password using the secrets module
-    password = ''.join(secrets.choice(characters) for i in range(length))
+    # Start with an empty list of characters
+    characters = []
+
+    # Add letters to the list if specified
+    if letters:
+        characters += string.ascii_letters
+
+    # Add digits to the list if specified
+    if digits:
+        characters += string.digits
+
+    # Add punctuation to the list if specified
+    if punctuation:
+        characters += string.punctuation
+
+    # Shuffle the list of characters to get a random order
+    random.shuffle(characters)
+    # Select the first `length` characters from the shuffled list
+    password = ''.join(characters[:length])
     return password
 
-# Generate a password with 20 characters
-password = generate_password(20)
+# Generate a password with 10 characters, including letters and digits
+password = generate_password(10, letters=True, digits=True, punctuation=False)
+print(password)
+
+# Generate a password with 15 characters, including letters, digits, and punctuation
+password = generate_password(15, letters=True, digits=True, punctuation=True)
 print(password)
